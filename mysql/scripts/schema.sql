1bb2958cf8b89ee1c517f48ad1618172c82d3ea0
prasad <prasad@gmail.com>
2024-08-11 07:55:10 +0530

    docker-expanse

---
+++ mysql/scripts/schema.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS transactions;
+USE transactions;
+
+CREATE TABLE IF NOT EXISTS transactions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    amount INT,
+    description VARCHAR(255)
+);
+
+CREATE USER IF NOT EXISTS 'expense'@'%' IDENTIFIED BY 'Prasad@123';
+GRANT ALL ON transactions.* TO 'expense'@'%';
+FLUSH PRIVILEGES;
